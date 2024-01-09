6ddfe8fb6b13b96e96585a6fc75c37a964c97d0d
Vasanth <68737119+vazand@users.noreply.github.com>
2024-01-09 23:39:30 +0530

    state Hook practice

--- src/Content.js
+++ src/Content.js
@@ -1,35 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Content() {
   function randomString() {
     const names = ["One Piece", "N̥aruto", "DBZ", "Death Note"];
     var ranNum = Math.floor(Math.random() * names.length);
-    return names[ranNum];
+    const currentVal = names[ranNum];
+    console.log(`current val = ${currentVal}`);
+    return currentVal;
   }
-  const changeAnime = (e) => {
-    //console.log(e);
-    // to check what is the target
-    //console.log(e.target);
-    // to get the inner text
-    console.log(e.target.innerText);
-  };
 
-  // tbis is not working! check again!
-  const towClick = (name) => {
-    console.log(`HAHAHA ${name}`);
-  };
+  const [count, setCount] = useState(50);
+  function increment() {
+    // Here all of the count value is the same
+    // so even we try to increase the setCount three times
+    // all of the count are same
+    // ex: if count = 12 then it applied for three times
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+
+    // if we actually want to update the count 3 time
+    // we have to use a anon function inside
+    //like this
+    setCount((param1) => {
+      return param1 + 1;
+    });
+    setCount((param2) => {
+      return param2 + 1;
+    });
+    setCount((param3) => {
+      return param3 + 1;
+    });
+  }
+  function decrement() {
+    setCount(count - 1);
+  }
+  function name() {
+    console.log("hahaha I am gonna slow the page");
+  }
+  // this will print again and again
+  // const [names, setname] = useState(name())
+  // this will print only once
+  const [names, setname] = useState(() => {
+    name();
+  });
+
   return (
-    <main className="text-center">
-      <p onDoubleClick={() => towClick(`SUPER`)}>click 2 times</p>
-      <p>Let's Watch {randomString()}</p>
-      {/* <button onClick={() => changeAnime("vazand")}>click</button> */}
+    <main className="text-center flex flex-col ">
+      <p>Let's Watch DBZ</p>
+
       <button
-        onClick={(e) => changeAnime(e)}
-        className="rounded-3xl bg-orange-400 text-white"
+        onClick={() => increment()}
+        className="rounded-lg bg-orange-400 text-white p-2 m-auto"
       >
-        Click Here
+        ADDITION
       </button>
-      <br></br>
+      <span>current Value {count}</span>
+      <button
+        onClick={() => decrement()}
+        className="rounded-lg bg-orange-400 text-white p-2 m-auto"
+      >
+        SUBTRACTION
+      </button>
     </main>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 function Header() {
   return (
     <header className="text-center">
-      <h1 className="bg-slate-500 text-green-500 ">A TODO LIST APP</h1>
+      <h1 className="bg-slate-500 text-white ">A TODO LIST APP</h1>
     </header>
   );
 }
